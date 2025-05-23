690f69973421417945ad7beb518d2b64cb7891a3
Camilo da Silveira <silveiracamilo@gmail.com>
2025-05-23 18:17:02 -0300

    feature: add logo and header bg

---
+++ src/core/@types/assets.d.ts
@@ -0,0 +1,14 @@
+declare module "*.png" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.ttf" {
+  const value: string;
+  export = value;
+}

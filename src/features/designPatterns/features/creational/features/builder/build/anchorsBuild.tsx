e378de64524a5b5f9fa8087547e4f037d295543f
Camilo da Silveira <silveiracamilo@gmail.com>
2025-05-23 14:10:33 -0300

    refactor(Builder): update title first anchor

--- src/features/designPatterns/features/creational/features/builder/build/anchorsBuild.tsx
+++ src/features/designPatterns/features/creational/features/builder/build/anchorsBuild.tsx
@@ -2,7 +2,7 @@ import { anchorsBuilder } from "@/features/designPatterns/builder/anchorsBuilder
 
 export const anchorsBuild = () => {
     return anchorsBuilder()
-        .addAnchor('part-1', 'Factory Method em Programação Funcional')
+        .addAnchor('part-1', 'Builder em Programação Funcional')
         .addAnchor('part-2', 'Características no paradigma funcional')
         .addAnchor('part-3', 'Exemplo')
         .addAnchor('part-4', 'Exemplo Render')
